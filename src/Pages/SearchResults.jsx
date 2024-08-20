f4a9d51a0124dedfad74377ca6060d712c6056dc
Jay-Rivera <119644467+Jay-Rivera@users.noreply.github.com>
2024-08-19 18:39:13 -0700

    made styling changes

--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Search from "../Components/Search";
 import axios from "axios";
 import { Link, useParams, useNavigate } from "react-router-dom";
+import CardImage from "../assets/Spotlight-img.webp";
 
 function SearchResults() {
   // let navigate = useNavigate()
@@ -24,7 +25,37 @@ function SearchResults() {
               Search results for 'movie title' :
             </h2>
           </div>
-          <div className="movie__card"></div>
+          <div className="movie__cards">
+            <div className="movie__card">
+              <img src={CardImage} alt="" />
+              <h1 className="movie__card--title">Title of movie</h1>
+              <p className="movie__card--description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                quidem reiciendis ipsum suscipit! Alias, et delectus ex dolor ea
+                cumque!
+              </p>
+            </div>
+            <div className="movie__card">
+              <img src={CardImage} alt="" />
+              <h1 className="movie__card--title">Title of movie</h1>
+              <p className="movie__card--description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                quidem reiciendis ipsum suscipit! Alias, et delectus ex dolor ea
+                cumque!
+              </p>
+            </div>
+            <div className="movie__card">
+              <div className="movie__card--image">
+                <img src={CardImage} alt="" />
+              </div>
+              <h1 className="movie__card--title">Title of movie</h1>
+              <p className="movie__card--description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                quidem reiciendis ipsum suscipit! Alias, et delectus ex dolor ea
+                cumque!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
